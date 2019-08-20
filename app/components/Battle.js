499fdd455e4c8b9f8393782216ad671492b2ba6f
shomail <shomail1988@gmail.com>
2019-08-20 09:11:22 +0200

    refactor PlayerPreview

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -7,7 +7,7 @@ import {
 } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { ThemeConsumer } from '../contexts/theme';
+import ThemeContext, { ThemeConsumer } from '../contexts/theme';
 
 const Instructions = () => (
   <ThemeConsumer>
@@ -104,35 +104,33 @@ PlayerInput.propTypes = {
   label: PropTypes.string.isRequired,
 };
 
-const PlayerPreview = ({ username, onReset, label }) => (
-  <ThemeConsumer>
-    {theme => (
-      <div className="column player">
-        <h3 className="player-label">{label}</h3>
-        <div className={`row bg-${theme}`}>
-          <div className="player-info">
-            <img
-              className="avatar-small"
-              src={`http://github.com/${username}.png?size=200`}
-              alt={`Avatar for ${username}`}
-            />
-            <a href={`http://github.com/${username}`} className="link">
-              {username}
-            </a>
-          </div>
-          <button
-            type="button"
-            className="btn-clear flex-center"
-            onClick={onReset}
-          >
-            <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
-          </button>
+const PlayerPreview = ({ username, onReset, label }) => {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className={`row bg-${theme}`}>
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`http://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`http://github.com/${username}`} className="link">
+            {username}
+          </a>
         </div>
+        <button
+          type="button"
+          className="btn-clear flex-center"
+          onClick={onReset}
+        >
+          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+        </button>
       </div>
-    )}
-  </ThemeConsumer>
-);
-
+    </div>
+  );
+};
 PlayerPreview.propTypes = {
   username: PropTypes.string.isRequired,
   onReset: PropTypes.func.isRequired,
