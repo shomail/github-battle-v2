8b3a3e7a94ae9223994d71e7e90d28525512c9aa
shomail <shomail1988@gmail.com>
2019-08-20 09:23:03 +0200

    refactor Instructions

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -7,43 +7,38 @@ import {
 } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import ThemeContext, { ThemeConsumer } from '../contexts/theme';
+import ThemeContext from '../contexts/theme';
 
-const Instructions = () => (
-  <ThemeConsumer>
-    {theme => (
-      <div className="instructions-container">
-        <h1 className="center-text header-sm">Instructions</h1>
-        <ol className="container-sm grid center-text battle-instructions">
-          <li>
-            <h3 className="header-sm">Enter two Github Users</h3>
-            <FaUserFriends
-              className={`bg-${theme}`}
-              color="rgb(255,191,116)"
-              size={140}
-            />
-          </li>
-          <li>
-            <h3 className="header-sm">Battle</h3>
-            <FaFighterJet
-              className={`bg-${theme}`}
-              color="#727272"
-              size={140}
-            />
-          </li>
-          <li>
-            <h3 className="header-sm">See the Winners</h3>
-            <FaTrophy
-              className={`bg-${theme}`}
-              color="rgb(255,215,0)"
-              size={140}
-            />
-          </li>
-        </ol>
-      </div>
-    )}
-  </ThemeConsumer>
-);
+function Instructions() {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <div className="instructions-container">
+      <h1 className="center-text header-sm">Instructions</h1>
+      <ol className="container-sm grid center-text battle-instructions">
+        <li>
+          <h3 className="header-sm">Enter two Github Users</h3>
+          <FaUserFriends
+            className={`bg-${theme}`}
+            color="rgb(255,191,116)"
+            size={140}
+          />
+        </li>
+        <li>
+          <h3 className="header-sm">Battle</h3>
+          <FaFighterJet className={`bg-${theme}`} color="#727272" size={140} />
+        </li>
+        <li>
+          <h3 className="header-sm">See the Winners</h3>
+          <FaTrophy
+            className={`bg-${theme}`}
+            color="rgb(255,215,0)"
+            size={140}
+          />
+        </li>
+      </ol>
+    </div>
+  );
+}
 
 function PlayerInput({ onSubmit, label }) {
   const [username, setusername] = React.useState('');
