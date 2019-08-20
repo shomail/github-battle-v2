7b6c04d50fbfd8eed4ae6019075ecb0da5566a0e
shomail <shomail1988@gmail.com>
2019-08-20 09:43:05 +0200

    refactor Card and Nav to useContext

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,47 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { ThemeConsumer } from '../contexts/theme';
+import PropTypes from 'prop-types';
+import ThemeContext from '../contexts/theme';
 
 const activeStyle = {
   color: 'lightcoral',
 };
 
 export default function Nav({ toggleTheme }) {
+  const theme = React.useContext(ThemeContext);
   return (
-    <ThemeConsumer>
-      {theme => (
-        <nav className="row space-between">
-          <ul className="row nav">
-            <li>
-              <NavLink
-                exact
-                activeStyle={activeStyle}
-                to="/"
-                className="nav-link"
-              >
-                Popular
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                activeStyle={activeStyle}
-                to="/battle"
-                className="nav-link"
-              >
-                Battle
-              </NavLink>
-            </li>
-          </ul>
-          <button
-            type="button"
-            style={{ fontSize: 30 }}
-            className="btn-clear"
-            onClick={toggleTheme}
-          >
-            {theme === 'light' ? '🔦' : '💡'}
-          </button>
-        </nav>
-      )}
-    </ThemeConsumer>
+    <nav className="row space-between">
+      <ul className="row nav">
+        <li>
+          <NavLink exact activeStyle={activeStyle} to="/" className="nav-link">
+            Popular
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={activeStyle} to="/battle" className="nav-link">
+            Battle
+          </NavLink>
+        </li>
+      </ul>
+      <button
+        type="button"
+        style={{ fontSize: 30 }}
+        className="btn-clear"
+        onClick={toggleTheme}
+      >
+        {theme === 'light' ? '🔦' : '💡'}
+      </button>
+    </nav>
   );
 }
+
+Nav.propTypes = {
+  toggleTheme: PropTypes.func,
+};
